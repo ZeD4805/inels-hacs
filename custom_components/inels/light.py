ca79d8ad0f69557cac6a0b6e1793d30f1d4e4337
ZeD4805 <zed4805@gmail.com>
2022-11-23 17:02:48 +0100

    generalized light platform

--- custom_components/inels/light.py
+++ custom_components/inels/light.py
@@ -33,15 +33,15 @@ async def async_setup_entry(
 
     for device in device_list:
         if device.device_type == Platform.LIGHT:
-            if device.inels_type == RFDAC_71B:
-                entities.append(InelsLight(device))
-            elif device.inels_type == DA3_22M:
+            if device.inels_type == DA3_22M:
                 entities.append(
                     InelsLightChannel(device, InelsLightChannelDescription(2, 0))
                 )
                 entities.append(
                     InelsLightChannel(device, InelsLightChannelDescription(2, 1))
                 )
+            else:
+                entities.append(InelsLight(device))
 
     async_add_entities(entities)
 
