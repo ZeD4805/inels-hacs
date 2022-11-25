156e0cbc3a3e0fa066b3b503ce8ee8b77a8fd169
zed4805 <zed4805@gmail.com>
2022-11-25 12:14:57 +0100

    removed SA3_01B temp sensor as a sensor

--- custom_components/inels/sensor.py
+++ custom_components/inels/sensor.py
@@ -322,15 +322,15 @@ async def async_setup_entry(
 
             for description in descriptions:
                 entities.append(InelsSensor(device, description=description))
-        if device.device_type == Platform.SWITCH:
-            if device.inels_type == SA3_01B:
-                descriptions = SENSOR_DESCRIPTION_TEMPERATURE_GENERIC
-
-            LOGGER.info("Trying to add SA3_01B device as a sensor...")
-            LOGGER.info(device.state)
-
-            for description in descriptions:
-                entities.append(InelsSensor(device, description=description))
+        # if device.device_type == Platform.SWITCH:
+        #    if device.inels_type == SA3_01B:
+        #        descriptions = SENSOR_DESCRIPTION_TEMPERATURE_GENERIC
+        #
+        #    LOGGER.info("Trying to add SA3_01B device as a sensor...")
+        #    LOGGER.info(device.state)
+        #
+        #    for description in descriptions:
+        #        entities.append(InelsSensor(device, description=description))
 
     async_add_entities(entities, True)
 
