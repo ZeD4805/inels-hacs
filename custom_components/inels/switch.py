2e30f769257f9be36560eb20777e696a59b31d08
zed4805 <zed4805@gmail.com>
2022-11-26 11:29:16 +0100

    added fix for switch behaviour

--- custom_components/inels/switch.py
+++ custom_components/inels/switch.py
@@ -39,19 +39,13 @@ async def async_setup_entry(
             if device.inels_type == RFSC_61:
                 entities.append(InelsSwitch(device=device))
             elif device.inels_type == SA3_01B:
-                entities.append(InelsSwitch(device=device))
+                entities.append(InelsComplexSwitch(device=device))
             # elif device.inels_type == GTR3_50:
             #    entities.append(InelsSwitch(device=device))
             # elif device.inels_type == GSB3_90SX:
             #    entities.append(InelsSwitch(device=device))
 
-    async_add_entities(
-        [
-            InelsSwitch(device)
-            for device in device_list
-            if device.device_type == Platform.SWITCH
-        ],
-    )
+    async_add_entities(entities)
 
 
 class InelsSwitch(InelsBaseEntity, SwitchEntity):
@@ -102,6 +96,70 @@ class InelsSwitch(InelsBaseEntity, SwitchEntity):
         if not self._device.is_available:
             return None
         await self.hass.async_add_executor_job(self._device.set_ha_value, True)
+
+    def _callback(self, new_value: Any) -> None:
+        """Get callback data from the broker."""
+        super()._callback(new_value)
+        # if self._state_attrs is not None:
+        #     if isinstance(self._device.state, bool) is False:
+        #         self._state_attrs[ATTR_TEMPERATURE] = self._device.state.temperature
+        #         self._state_attrs["on"] = self._device.state.on
+
+
+class InelsComplexSwitch(InelsBaseEntity, SwitchEntity):
+    """The platform class required by Home Assistant."""
+
+    def __init__(self, device: Device) -> None:
+        """Initialize a switch."""
+        super().__init__(device=device)
+
+        # self._state_attrs = None
+
+        # if isinstance(device.state, bool) is False:
+        #     if hasattr(device.state, "on"):
+        #         self._state_attrs = {
+        #             ATTR_TEMPERATURE: device.state.temperature,
+        #             "on": device.state.on,
+        #         }
+
+    @property
+    def is_on(self) -> bool:
+        """Return true if switch is on."""
+        # if isinstance(self._device.state, bool) is False:
+        #     if hasattr(self._device.state, "on"):
+        #         return self._device.state.on
+
+        return self._device.state.on
+
+    @property
+    def icon(self) -> str | None:
+        """Switch icon."""
+        return ICON_SWITCH
+
+    # @property
+    # def extra_state_attributes(self) -> Mapping[str, Any] | None:
+    #     """Extra attributes if exists."""
+    #     if self._state_attrs is None:
+    #         return super().extra_state_attributes
+    #     return self._state_attrs
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Instruct the switch to turn off."""
+        if not self._device.is_available:
+            return None
+
+        ha_val = self._device.get_value().ha_value
+        ha_val.on = False
+        await self.hass.async_add_executor_job(self._device.set_ha_value, ha_val)
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Instruct the switch to turn on."""
+        if not self._device.is_available:
+            return None
+
+        ha_val = self._device.get_value().ha_value
+        ha_val.on = True
+        await self.hass.async_add_executor_job(self._device.set_ha_value, ha_val)
 
     def _callback(self, new_value: Any) -> None:
         """Get callback data from the broker."""
