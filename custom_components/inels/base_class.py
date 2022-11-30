b309d23215b75529f116d98af2fbfce7049775d7
zed4805 <zed4805@gmail.com>
2022-11-30 07:27:02 +0100

    fixed typo in docstring

--- custom_components/inels/base_class.py
+++ custom_components/inels/base_class.py
@@ -58,7 +58,7 @@ class InelsBaseEntity(Entity):
 
     @property
     def available(self) -> bool:
-        """Return if entity si available."""
+        """Return if entity is available."""
         if self._device.is_subscribed is False:
             self._device.mqtt.subscribe(self._device.state_topic)
 
