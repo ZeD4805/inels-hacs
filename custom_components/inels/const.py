458aa1db5fa36ac992b56af26a0832539431d1ab
zed4805 <zed4805@gmail.com>
2022-12-01 10:45:15 +0100

    pls fix everything

--- custom_components/inels/const.py
+++ custom_components/inels/const.py
@@ -3,7 +3,7 @@ import logging
 
 from homeassistant.const import Platform
 
-DOMAIN = "inels-dev"
+DOMAIN = "inels"
 
 BROKER_CONFIG = "inels_mqtt_broker_config"
 BROKER = "inels_mqtt_broker"
@@ -11,7 +11,7 @@ DEVICES = "devices"
 
 CONF_DISCOVERY_PREFIX = "discovery_prefix"
 
-TITLE = "iNELS-dev"
+TITLE = "iNELS"
 DESCRIPTION = ""
 INELS_VERSION = 1
 LOGGER = logging.getLogger(__package__)
